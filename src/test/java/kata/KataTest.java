976b5f7e094de530d0650e4e5cfab38b253c3454
Xavier Pigeon <craftsman@chrysocode.io>
2019-05-10 17:02:38 +0200

    It should find a commond year given 2019.
    - Transform {} into nil and nil into constant.
    - Fake it until you make it.

--- src/test/java/kata/KataTest.java
+++ src/test/java/kata/KataTest.java
@@ -24,29 +24,15 @@ package kata;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static testasyouthink.TestAsYouThink.givenSut;
 import static testasyouthink.TestAsYouThink.resultOf;
-import static testasyouthink.TestAsYouThink.when;
 
 class KataTest {
 
     @Test
-    void should_behave_given_context() {
-        // Choose the proper syntax for your test case.
-
-        resultOf(() -> "TestAsYouThink").isEqualTo("TestAsYouThink");
-
-        when(() -> "TestAsYouThink").then(result -> {
-            assertThat(result)
-                    .startsWith("Test")
-                    .hasSize(14);
-        });
-
-        givenSut(StringBuilder::new)
-                .whenSutRuns(sut -> sut
-                        .append("TestAsYouThink")
-                        .reverse())
-                .then(sut -> assertThat(sut).containsSequence("knihTuoYsAtseT"));
+    void should_find_a_common_year_given_2019() {
+        resultOf(() -> {
+            int year = 2019;
+            return false;
+        }).isFalse();
     }
 }
